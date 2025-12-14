a19e6d7c13ee8e524f95abee5ad4453f8b318d7b
skostadinov0141 <simeon.kostadinov0141@gmail.com>
2025-12-14 14:27:34 +0100

    chore: adjusted base entity class and added crud operations with mongodb client in user.go
    - removed SaveMultiple function from base entity
    - added Delete function to base entity
    - added generic type to base entity

--- core/entity.go
+++ core/entity.go
@@ -1,16 +1,18 @@
 package core
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Entity struct {
-	ID        string `bson:"_id"`
-	CreatedAt int64  `bson:"created_at"`
-	UpdatedAt int64  `bson:"updated_at"`
+	ID        primitive.ObjectID `bson:"_id"`
+	CreatedAt int64              `bson:"created_at"`
+	UpdatedAt int64              `bson:"updated_at"`
 }
 
-type IEntity interface {
+type IEntity[T any] interface {
 	CollectionName() string
-	CreateNewEntity() *Entity
-	LoadByID(id string) *Entity
-	LoadByIDs(ids []string) []*Entity
-	SaveOne(entity *Entity) error
-	SaveMany(entities []*Entity) error
+	CreateNew() *T
+	LoadByID(id string) *T
+	LoadByIDs(ids []string) []*T
+	Save() error
+	Delete() error
 }
--- entities/user.go
+++ entities/user.go
@@ -1,48 +1,90 @@
 package entities
 
 import (
+	"context"
+	"time"
+
 	"github.com/keyloom/web-api/core"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type User struct {
-	core.Entity `bson:",inline"`
-	Email       string `bson:"email,email"`
-	Password    string `bson:"password,containsany=uppercase,containsany=lowercase,containsany=numeric,min=8"`
+	core.Entity `json:",inline" bson:",inline"`
+	Email       string `json:"email" bson:"email"`
+	Password    string `json:"password" bson:"password,containsany=uppercase,containsany=lowercase,containsany=numeric,min=8"`
 }
 
-var _ core.IEntity = (*User)(nil)
+var _ core.IEntity[User] = (*User)(nil)
 
 func (u *User) CollectionName() string {
 	return "users"
 }
 
-func (u *User) CreateNewEntity() *core.Entity {
-	// Placeholder implementation
-	return &core.Entity{}
-}
-
-func (u *User) LoadByID(id string) *core.Entity {
-	// Placeholder implementation
-	return &core.Entity{ID: id}
-}
-
-func (u *User) LoadByIDs(ids []string) []*core.Entity {
-	// Placeholder implementation
-	entities := make([]*core.Entity, len(ids))
-	for i, id := range ids {
-		entities[i] = &core.Entity{ID: id}
+func (u *User) CreateNew() *User {
+	return &User{
+		Entity: core.Entity{
+			ID:        primitive.NilObjectID,
+			CreatedAt: time.Now().Unix(),
+			UpdatedAt: time.Now().Unix(),
+		},
 	}
-	return entities
 }
 
-func (u *User) SaveOne(entity *core.Entity) error {
-	// Placeholder implementation
-	return nil
+func (u *User) LoadByID(id string) *User {
+	client := core.NewMongoClient()
+	result := client.FindOne(u.CollectionName(), bson.M{"_id": id})
+	if result.Err() != nil {
+		return nil
+	}
+	var user User
+	err := result.Decode(&user)
+	if err != nil {
+		return nil
+	}
+	return &user
 }
 
-func (u *User) SaveMany(entities []*core.Entity) error {
-	// Placeholder implementation
-	return nil
+// Loads multiple users by their IDs
+func (u *User) LoadByIDs(ids []string) []*User {
+	client := core.NewMongoClient()
+	cursor, err := client.FindMany(u.CollectionName(), bson.M{
+		"_id": bson.M{"$in": ids},
+	})
+	if err != nil {
+		return nil
+	}
+	defer cursor.Close(context.TODO())
+
+	var users []*User
+	for cursor.Next(context.TODO()) {
+		var user User
+		if err := cursor.Decode(&user); err != nil {
+			continue
+		}
+		users = append(users, &user)
+	}
+	return users
+}
+
+// Saves the user to the database
+func (u *User) Save() error {
+	client := core.NewMongoClient()
+	if u.ID != primitive.NilObjectID {
+		_, err := client.UpdateOne(u.CollectionName(), bson.M{"_id": u.ID}, bson.M{"$set": u})
+		return err
+	} else {
+		u.ID = primitive.NewObjectID()
+		_, err := client.InsertOne(u.CollectionName(), u)
+		return err
+	}
+}
+
+// Deletes the user from the database
+func (u *User) Delete() error {
+	client := core.NewMongoClient()
+	_, err := client.DeleteOne(u.CollectionName(), bson.M{"_id": u.ID})
+	return err
 }
 
 // Hashes and sets the user's password
